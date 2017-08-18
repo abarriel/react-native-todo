f9be960eb6ba04451dc597147adb77fe621ecc2e
Allan BARRIELLE <abarriel@student.42.fr>
2017-08-18 16:50:13 +0200

    add linter

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    flex: 1,
+    backgroundColor: 'powderblue',
+  },
+  main: {
+    flex: 2,
+    backgroundColor: 'skyblue',
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: 'steelblue',
   },
 });
+
+const App = () => (
+  <View style={styles.container}>
+    <View style={styles.header}>
+    </View>
+    <View style={styles.main}>
+      <Text>Hello World Mehdi</Text>
+    </View>
+    <View style={styles.footer}>
+    </View>
+  </View>
+);
+
+export default App;
